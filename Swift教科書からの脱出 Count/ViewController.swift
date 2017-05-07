ef9531231920d1ebe3cdd568e2f36951080dc15c
A.F <akki_mawile23@yahoo.co.jp>
2017-05-07 16:17:44 +0900

    はじまり

--- Swift教科書からの脱出 Count/ViewController.swift
+++ Swift教科書からの脱出 Count/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count:Int!
+    var number:Int!
+    @IBOutlet var instructionLabel:UILabel!
+    @IBOutlet var countLabel:UILabel!
+    @IBOutlet var numberLabel:UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        instructionLabel.numberOfLines = 2
+        instructionLabel.text = "11回で獣の数字を導け。\nそうすれば鍵が手に入る。"
+        number = 0
+        count = 0
+        numberLabel.text = String(number)
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        count = count + 1
+        numberLabel.text = String(number)
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        count = count + 1
+        numberLabel.text = String(number)
+        countLabel.text = String(count)
+    }
+    
+    @IBAction func times(){
+        number = number * 2
+        count = count + 1
+        numberLabel.text = String(number)
+        countLabel.text = String(count)
     }
 
     override func didReceiveMemoryWarning() {
